732d8a564fa5a6efe648a688c436a3abcc67af2d
Cristi <498935434@qq.com>
2017-02-20 14:47:29 +0800

    resume

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+$(function(){
+
+    jQuery(document).ready(function(){
+        
+        $("body").backstretch([
+                "images/bg1.jpg",
+                "images/bg2.jpg",
+                "images/bg3.jpg"
+                ],{duration:3000,fade:1300}
+        	);
+
+    });
+
+
+    jQuery(document).ready(function(){
+        
+        $(".education").backstretch([
+                "images/campus1.jpg",
+                "images/campus2.jpg",
+                "images/campus3.jpg"
+                ],{duration:3000,fade:1300}
+        	);
+
+    });
+
+
+})
